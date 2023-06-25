7217f4722908f5a6dde4dc7e4c3660d75aff34be
Newton <nkafle49@gmail.com>
2023-06-25 20:04:28 +0930

    Test case and db connection django command completed

--- app/app/calc.py
+++ app/app/calc.py
@@ -3,9 +3,11 @@ calculator functions
 
 """
 
+
 def add(x, y):
     """Add x and y and return result."""
     return x + y
+
 
 def subtract(x, y):
     """subtract x from y and return the result"""
